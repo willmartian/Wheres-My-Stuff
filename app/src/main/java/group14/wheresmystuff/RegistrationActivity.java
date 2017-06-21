f70f9b63e5be26ef3614db561f8d258145e72963
Julie Pierides <juliepierides@gatech.edu>
2017-06-21 19:23:11 -0400

    registration page and buttons created

---
+++ app/src/main/java/group14/wheresmystuff/RegistrationActivity.java
@@ -0,0 +1,48 @@
+package group14.wheresmystuff;
+
+import android.support.v7.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.content.Intent;
+
+
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+        Button register = (Button) findViewById(R.id.register_button);
+        Button cancel = (Button) findViewById(R.id.cancel_button);
+        cancel.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToSplashActivity();
+            }
+        });
+        register.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                goToLoginActivity();
+
+            }
+
+        });
+    }
+
+    private void goToLoginActivity(){
+        Intent intent = new Intent(this,LoginActivity.class);
+        startActivity(intent);
+    }
+    private void goToSplashActivity(){
+        Intent intent = new Intent(this, SplashActivity.class);
+        startActivity(intent);
+    }
+
+}
