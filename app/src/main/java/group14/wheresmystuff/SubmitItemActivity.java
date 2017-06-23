6a59784872ef7d123ba9086a92091ed7864b191c
richardtz12 <rzhang341@gatech.edu>
2017-06-22 20:10:26 -0400

    Added the submititem xml and SubmititemActivity.java classes and made the ghost text on the registration page all start with a capital letter.

---
+++ app/src/main/java/group14/wheresmystuff/SubmitItemActivity.java
@@ -0,0 +1,24 @@
+package group14.wheresmystuff;
+
+import android.support.v7.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.content.Intent;
+
+
+/**
+ * Created by Richard on 6/22/2017.
+ */
+
+public class SubmitItemActivity {
+    public class RegistrationActivity extends AppCompatActivity {
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_submititem);
+
+        }
+    }
+}
