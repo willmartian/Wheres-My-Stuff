ccf49b8ece9d764846c2116dd44fe700e32a65c7
Will Martin <williammartin@gatech.edu>
2017-06-26 23:37:39 -0400

    added registration functionality

--- app/src/main/java/group14/wheresmystuff/RegistrationActivity.java
+++ app/src/main/java/group14/wheresmystuff/RegistrationActivity.java
@@ -13,11 +13,20 @@ import android.widget.EditText;
 
 public class RegistrationActivity extends AppCompatActivity {
 
+    private EditText mNameView;
+    private EditText mPasswordView;
+    private EditText mUserIDView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+        //
+        mNameView = (EditText) findViewById(R.id.name_textBox);
+        mPasswordView = (EditText) findViewById(R.id.password_textBox);
+        mUserIDView = (EditText) findViewById(R.id.username_textBox);
+
         Button register = (Button) findViewById(R.id.register_button);
         Button cancel = (Button) findViewById(R.id.cancel_button);
         cancel.setOnClickListener(new OnClickListener() {
@@ -31,6 +40,10 @@ public class RegistrationActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
+                String userID = mUserIDView.getText().toString();
+                String password = mPasswordView.getText().toString();
+                String name = mNameView.getText().toString();
+                Model.getUserList().add(new User(name, userID, password, ""));
                 goToLoginActivity();
 
             }
