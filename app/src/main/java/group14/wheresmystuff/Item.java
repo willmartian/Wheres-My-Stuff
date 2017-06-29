1ec2d786d6fb43e10d7accb96f017a4e2c37692e
Julie Lee <jlee0402@lawn-143-215-123-120.lawn.gatech.edu>
2017-06-29 13:14:39 -0400

    added javadocs updated

--- app/src/main/java/group14/wheresmystuff/Item.java
+++ app/src/main/java/group14/wheresmystuff/Item.java
@@ -47,74 +47,146 @@ public class Item {
         return itemType.toString() + ": \"" + name + "\" in " + location;
     }
 
+    /**
+     * getter for name
+     * @return String gets and returns the name of the user
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * setter for name
+     * @param name
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * getter for description
+     * @return String gets and returns the description
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     * setter for description
+     * @param description
+     */
     public void setDescription(String description) {
         this.description = description;
     }
 
+    /**
+     * getter for location
+     * @return String gets and returns the location
+     */
     public String getLocation() {
         return location;
     }
 
+    /**
+     * setter for location
+     * @param location
+     */
     public void setLocation(String location) {
         this.location = location;
     }
 
+    /**
+     * getter for creator
+     * @return String gets and returns creator
+     */
     public User getCreator() {
         return creator;
     }
 
+    /**
+     * setter for creator
+     * @param creator
+     */
     public void setCreator(User creator) {
         this.creator = creator;
     }
 
+    /**
+     * getter for date
+     * @return String gets and returns the date
+     */
     public Date getDate() {
         return date;
     }
 
+    /**
+     * setter for date
+     * @param date
+     */
     public void setDate(Date date) {
         this.date = date;
     }
 
+    /**
+     * getter for open
+     * @return boolean if true, gets and returns open
+     */
     public boolean isOpen() {
         return open;
     }
 
+    /**
+     * setter for open
+     * @param open
+     */
     public void setOpen(boolean open) {
         this.open = open;
     }
 
+    /**
+     * getter for category
+     * @return Category gets and returns the category
+     */
     public Category getCategory() {
         return category;
     }
 
+    /**
+     * setter for category
+     * @param category
+     */
     public void setCategory(Category category) {
         this.category = category;
     }
 
+    /**
+     * getter for item type
+     * @return ItemType gets and returns the item type
+     */
     public ItemType getItemType() {
         return itemType;
     }
 
+    /**
+     * setter for item type
+     * @param itemType
+     */
     public void setItemType(ItemType itemType) {
         this.itemType = itemType;
     }
 
+    /**
+     * getter for reward
+     * @return double gets and returns reward
+     */
     public double getReward() {
         return reward;
     }
 
+    /**
+     * setter for reward
+     * @param reward
+     */
     public void setReward(double reward) {
         this.reward = reward;
     }
